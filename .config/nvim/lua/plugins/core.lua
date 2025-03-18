8fff19520dfdf616f8db22239737987bd269e270
Maximilian <mpinderwhite@gmail.com>
2025-03-18 08:21:36 +0100

    feat: update lazy config

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -1,7 +1,3 @@
 return {
-  {
-    "alexwu/nvim-snazzy",
-    lazy = true,
-    name = "snazzy",
-  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, colorscheme = "catppuccin-macchiato" },
 }
