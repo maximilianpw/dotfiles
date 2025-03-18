bd4795719b4ff833194feb0f8f41aee12133509e
Maximilian <mpinderwhite@gmail.com>
2025-03-18 08:21:36 +0100

    feat: update lazy config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -37,6 +37,7 @@ require("lazy").setup({
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
+    colorscheme = "catppuccin-macchiato",
   },
   checker = {
     enabled = true, -- check for plugin updates periodically
