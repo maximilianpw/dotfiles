6add5ef04f9883f0650641b58e1b3702f97d0f4d
Maximilian <mpinderwhite@gmail.com>
2025-06-22 14:36:26 +0200

    feat(nvim): added new plugins for nvim

--- .config/nvim/lua/plugins/lsp/typescript.lua
+++ .config/nvim/lua/plugins/lsp/typescript.lua
@@ -9,5 +9,12 @@ return {
     event = 'VeryLazy',
     opts = {},
   },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'mlaursen/vim-react-snippets',
+    },
+  },
 }
-
