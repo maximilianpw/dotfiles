e47bd0c4bd165da98a940f22f4a4fea72dafc29a
Maximilian <mpinderwhite@gmail.com>
2025-06-23 15:11:14 +0200

    added fish config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -111,11 +111,14 @@ require('lazy').setup({
     },
   },
   {
-    'rebelot/kanagawa.nvim',
-    version = '*',
-    priority = 2000,
+    'navarasu/onedark.nvim',
+    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      vim.cmd.colorscheme 'kanagawa-wave'
+      require('onedark').setup {
+        style = 'darker',
+      }
+      -- Enable theme
+      require('onedark').load()
     end,
   },
   -- Highlight todo, notes, etc in comments
