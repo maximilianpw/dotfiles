1d89a160991dc31b206b67442c9b73f6bb22586a
Maximilian <mpinderwhite@gmail.com>
2025-06-23 15:11:14 +0200

    added fish config

--- .config/nvim/lua/plugins/java/java.lua
+++ .config/nvim/lua/plugins/java/java.lua
@@ -1,1 +1,44 @@
-return { 'nvim-java/nvim-java' }
+return {
+  'nvim-java/nvim-java',
+  config = false,
+  dependencies = {
+    {
+      'neovim/nvim-lspconfig',
+      opts = {
+        servers = {
+          jdtls = {
+            settings = {
+              java = {
+                configuration = {
+                  runtimes = {
+                    {
+                      name = 'JavaSE-23',
+                      path = '/opt/homebrew/opt/openjdk/libexec/openjdk.jdk',
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        setup = {
+          jdtls = function()
+            -- Your nvim-java configuration goes here
+            require('java').setup {
+              root_markers = {
+                'settings.gradle',
+                'settings.gradle.kts',
+                'pom.xml',
+                'build.gradle',
+                'mvnw',
+                'gradlew',
+                'build.gradle',
+                'build.gradle.kts',
+              },
+            }
+          end,
+        },
+      },
+    },
+  },
+}
