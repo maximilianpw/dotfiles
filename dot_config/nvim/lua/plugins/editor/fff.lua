ab7e93281ca977aee8c66d86bb7ebccfb9f6c1bd
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2025-12-08 10:21:58 +0100

    fix: fff prompt

--- dot_config/nvim/lua/plugins/editor/fff.lua
+++ dot_config/nvim/lua/plugins/editor/fff.lua
@@ -1,21 +1,17 @@
 return {
-  "dmtrKovalenko/fff.nvim",
-  build = "cargo build --release",
-  keys = {
-    {
-      "<leader>ff",
-      function()
-        require("fff").find()
-      end,
-      desc = "Find Files (fff)",
-    },
-    {
-      "<leader>fc",
-      function()
-        require("fff").find({ base_path = "~/.local/share/chezmoi/dot_config/nvim" })
-      end,
-      desc = "Find Config Files",
-    },
-  },
-  opts = {},
+	"dmtrKovalenko/fff.nvim",
+	build = "cargo build --release",
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("fff").find()
+			end,
+			desc = "Find Files (fff)",
+		},
+	},
+	opts = {
+		prompt = "> ",
+		title = "Find Files",
+	},
 }
