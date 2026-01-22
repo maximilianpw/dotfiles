8dd4d71048a610eb660b4f282117713f8a2059a9
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2026-01-22 19:33:35 +0100

    add obsidian requirements

--- dot_config/nvim/lua/plugins/style/lint.lua
+++ dot_config/nvim/lua/plugins/style/lint.lua
@@ -6,7 +6,6 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'vale' },
         dockerfile = { 'hadolint' },
         terraform = { 'tflint' },
         text = { 'vale' },
