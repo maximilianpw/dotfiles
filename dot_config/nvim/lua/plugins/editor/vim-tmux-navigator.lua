c6b988e29e4711559cbf645312140a1b69242f74
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2026-02-03 14:02:34 +0100

    feat: tmux window switching

---
+++ dot_config/nvim/lua/plugins/editor/vim-tmux-navigator.lua
@@ -0,0 +1,10 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  event = "VeryLazy",
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate left (tmux/nvim)" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate down (tmux/nvim)" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate up (tmux/nvim)" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate right (tmux/nvim)" },
+  },
+}
