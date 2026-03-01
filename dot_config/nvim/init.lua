c041e28ba65b8c9b2b666e55411c76fce46f2aa7
Maximilian PINDER-WHITE <mpinderwhite@proton.me>
2026-03-01 22:17:16 +0100

    theme

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -17,15 +17,12 @@ require("lazy").setup({
 	{ "tpope/vim-sleuth", event = "VeryLazy" }, -- Detect tabstop and shiftwidth automatically
 
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
+		"sainnhe/everforest",
 		priority = 1000,
-		opts = {
-			flavour = "mocha",
-		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme("catppuccin")
+		config = function()
+			vim.g.everforest_background = "hard"
+			vim.g.everforest_better_performance = 1
+			vim.cmd.colorscheme("everforest")
 		end,
 	},
 	{
